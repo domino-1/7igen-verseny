965159b6db2f34b59655e44dcafa023d6c0ccfe8
domino-1 <78816259+domino-1@users.noreply.github.com>
2023-10-02 18:05:36 +0200

    temporarily removed the html parser bits

---
+++ app/(main)/kampanyrol/[slug]/page.jsx
@@ -0,0 +1,31 @@
+import { getHirPost, getCatSlugs } from '../../../../lib/api';
+import Link from 'next/link';
+//import ParsedHtml from '../../../../components/custom/parsed-html';
+import Breadcrumbs from '../../../../components/custom/breadcrumbs';
+//import ParsedHtml from '~/components/custom/parsed-html';
+//import { sanitize, isSupported } from "isomorphic-dompurify";
+//import parse from 'html-react-parser';
+//import PostBody from '../../../../components/post-body'
+
+export async function generateStaticParams() {
+    const hirSlugs = await getCatSlugs("kampanyrol")/*.then((res) => res.json())*/;
+
+    return hirSlugs.map((post) => ({
+        slug: post.slug,
+    }));
+}
+
+export default async function Page({ params }) {
+    const content = await getHirPost(params.slug);
+
+    const published = new Date(content.date);
+
+    return <>
+        <Breadcrumbs link={params.category} category={params.category} />
+        <h1>{content.title}</h1>
+        <p>By: {content.author.node.name} | {published.toDateString().slice(4)}</p>
+        <br />
+        {/*<PostBody content={DOMPurify.sanitize(content.content)}></PostBody>*/}
+        <div>{content.content}</div>
+    </>
+}
---
+++ app/(main)/kampanyrol/page.jsx
@@ -0,0 +1,6 @@
+export default function CategoryHomePage() {
+    return (<>
+        <h1 className="category-desc-page">A Kampányról</h1>
+        <p>Kategória leírás</p>
+    </>)
+}
