c5d2f85ad7e0d54217b03938e814e9c1e3535617
domino-1 <78816259+domino-1@users.noreply.github.com>
2023-10-09 11:28:18 +0200

    removed purifier temporarily

--- components/custom/parsed-html.jsx
+++ components/custom/parsed-html.jsx
@@ -1,9 +1,9 @@
-import DOMPurify, { sanitize, isSupported } from "dompurify";
+//import * as DOMPurify from "dompurify";
 import parse from 'html-react-parser';
 
 export default function ParsedHtml({ htmlString }) {
-    const cleanHtml = DOMPurify.sanitize( htmlString );
-    const parsed = parse( cleanHtml );
+    //const cleanHtml = DOMPurify.sanitize( htmlString );
+    const parsed = parse( htmlString );
     //const parsed = cleanHtml;
 
     return <>{parsed}</>
