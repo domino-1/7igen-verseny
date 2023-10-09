b731bee066cc4423215b4636cf8f63aab4008bed
domino-1 <78816259+domino-1@users.noreply.github.com>
2023-10-09 11:24:02 +0200

    parser testing

--- components/custom/parsed-html.jsx
+++ components/custom/parsed-html.jsx
@@ -1,9 +1,9 @@
-//import DOMPurify, { sanitize, isSupported } from "isomorphic-dompurify";
+import DOMPurify, { sanitize, isSupported } from "dompurify";
 import parse from 'html-react-parser';
 
 export default function ParsedHtml({ htmlString }) {
-    //const cleanHtml = DOMPurify.sanitize( htmlString );
-    const parsed = parse( htmlString );
+    const cleanHtml = DOMPurify.sanitize( htmlString );
+    const parsed = parse( cleanHtml );
     //const parsed = cleanHtml;
 
     return <>{parsed}</>
