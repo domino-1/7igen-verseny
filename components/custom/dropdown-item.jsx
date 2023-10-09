9b4c30b28ff341f3e25ed7452c20c85b455662de
domino-1 <78816259+domino-1@users.noreply.github.com>
2023-10-09 15:01:31 +0200

    try again

--- components/custom/dropdown-item.jsx
+++ components/custom/dropdown-item.jsx
@@ -2,10 +2,11 @@
 
 import Link from 'next/link';
 import styles from './dropdown.module.css';
+import ParsedHtml from './parsed-html';
 
 export default function DropdownItem({ target, name, description}) {
     return  <Link href={target !== undefined ? target : "/"}>
                 <span>{ name }</span>
-                <span className={styles.description}>{ description }</span>
+                <span className={styles.description}><ParsedHtml htmlString={description} /></span>
             </Link>
 }
