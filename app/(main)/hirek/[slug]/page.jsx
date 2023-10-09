33bb4902f062930cb0edb6d5c5eb4af8a0dca243
domino-1 <78816259+domino-1@users.noreply.github.com>
2023-10-09 11:15:49 +0200

    testing parser

--- app/(main)/hirek/[slug]/page.jsx
+++ app/(main)/hirek/[slug]/page.jsx
@@ -1,6 +1,6 @@
 import { getHirPost, getCatSlugs } from '../../../../lib/api';
 import Link from 'next/link';
-//import ParsedHtml from '../../../../components/custom/parsed-html';
+import ParsedHtml from '../../../../components/custom/parsed-html';
 import Breadcrumbs from '../../../../components/custom/breadcrumbs';
 //import ParsedHtml from '~/components/custom/parsed-html';
 //import { sanitize, isSupported } from "isomorphic-dompurify";
@@ -27,7 +27,6 @@ export default async function Page({ params }) {
         <h1>{content.title}</h1>
         <p>By: {content.author.node.name} | {published.toDateString().slice(4)}</p>
         <br />
-        {/*<PostBody content={DOMPurify.sanitize(content.content)}></PostBody>*/}
-        <div>{/*<ParsedHtml htmlString={content.content} ></ParsedHtml>*/content.contet}</div>
+        <div>{<ParsedHtml htmlString={content.content} ></ParsedHtml>/*content.contet*/}</div>
     </>
 }
--- components/custom/parsed-html.jsx
+++ components/custom/parsed-html.jsx
@@ -1,9 +1,10 @@
 import DOMPurify, { sanitize, isSupported } from "isomorphic-dompurify";
-import parse from 'html-react-parser';
+//import parse from 'html-react-parser';
 
 export default function ParsedHtml({ htmlString }) {
     const cleanHtml = DOMPurify.sanitize( htmlString );
-    const parsed = parse(cleanHtml);
+    //const parsed = parse(cleanHtml);
+    const parsed = cleanHtml;
 
     return <>{parsed}</>
 }
