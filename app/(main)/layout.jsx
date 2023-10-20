0faf63200665286c6dce1cf6e42add9aa8caf6f7
domino-1 <78816259+domino-1@users.noreply.github.com>
2023-10-20 23:10:13 +0200

    trg _blnk

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -5,6 +5,6 @@ export default function MainLayout({ children }) {
     return (<>
         <TopBar />
         <main className="bodyContent" style={{minHeight: 100 + 'vh'}}>{children}</main>
-        <div style={{position: 'fixed',width: 100+'vw',bottom: 0,display: 'flex',justifyContent: 'center',zIndex: 2}}><Link href="https://eleve.hu/pc/7igen" style={{textDecoration: 'none',filter: 'drop-shadow(0px 0px 5px black)'}}><button className="hex-button" style={{color: 'white',background: 'var(--main-red)',textDecoration: 'none',padding: '1.5em 6em',clipPath: 'polygon(15% 0,85% 0,100% 100%,0% 100%)'}}>Szavazz weben!</button></Link></div>
+        <div style={{position: 'fixed',width: 100+'vw',bottom: 0,display: 'flex',justifyContent: 'center',zIndex: 2}}><Link href="https://eleve.hu/pc/7igen" target="_blank" style={{textDecoration: 'none',filter: 'drop-shadow(0px 0px 5px black)'}}><button className="hex-button" style={{color: 'white',background: 'var(--main-red)',textDecoration: 'none',padding: '1.5em 6em',clipPath: 'polygon(15% 0,85% 0,100% 100%,0% 100%)'}}>Szavazz weben!</button></Link></div>
     </>)
 }
