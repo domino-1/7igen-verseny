c16cfa868f605ef9422248059a8f510ec579dcb5
domino-1 <78816259+domino-1@users.noreply.github.com>
2023-10-28 04:41:23 +0200

    challenge links

--- app/(main)/challengek/page.jsx
+++ app/(main)/challengek/page.jsx
@@ -1,5 +1,6 @@
 import Breadcrumbs from "../../../components/custom/breadcrumbs";
 import ParsedHtml from "../../../components/custom/parsed-html";
+import Link from "next/link";
 
 export default async function CategoryHomePage() {
 
@@ -9,57 +10,59 @@ export default async function CategoryHomePage() {
         <br />
         <section class="feature" style={{marginTop: '2em'}}>
         <h2>Szavazás</h2>
-            <div className="challengeItem">
+            <Link href="https://forms.gle/TvwhqueZ8XYzfJdb8" className="challengeItem">
             <div><b>Szavazz!</b> - 1p</div>
             <ul>
                 <li>Élőben: Szelfi szavazás után</li>
                 <li>Online: Oszd meg, hogy szavaztál! A megosztott posztról csinálj egy screenshotot és küld be, vagy küld be a poszt linkjét</li>
                 <li>FONTOS: Tilos lefotózni a szavazólapot, vagy más szavazókat</li>
             </ul>
-            </div>
+            </Link>
             <br />
 
-            <div className="challengeItem">
+            <Link href="https://forms.gle/crR4HAEU4RCVvUAU8" className="challengeItem">
             <div><b>Hozz még egy szavazót!</b> - 2p (1p neki a "Szavazz!"-ért!) - Ismételhető</div>
             <ul>
                 <li>Élőben: Szelfizzetek együtt szavazás után!</li>
                 <li>Online: Osszátok meg mindketten, hogy szavaztatok! A megosztott posztrokról csináljatok egy screenshotot és küld be, vagy küld be a posztok linkjét</li>
                 <li>FONTOS: Tilos lefotózni a szavazólapot, vagy más szavazókat akik nem ti vagytok</li>
             </ul>
-            </div>
+            </Link>
             <br />
 
-            <div className="challengeItem">
+            <Link href="https://forms.gle/wRHPVMmuiRc11Jkr8" className="challengeItem">
             <div><b>Első szavazó</b> - 3p</div>
             <ul>
                 <li>Élőben: Fotózkodj az Első Szavazó matricáddal</li>
             </ul>
-            </div>
+            </Link>
             <br />
 
-            <div className="challengeItem">
+            <Link href="https://forms.gle/atW752rQf7h8wMG89" className="challengeItem">
             <div><b>Sulitúra</b> - 7p</div>
             <ul>
                 <li>Érjetek el minél több iskolát! A csapatodnak annyiszor 7 pont jár, ahány különböző iskolából vannak tagjai.</li>
                 <li>Online és Élőben: Ugyan az a rendszer mint a "Hozz még egy szavazót!"nál</li>
             </ul>
-            </div>
+            </Link>
             <br />
         </section>
 
         <section class="feature" style={{marginTop: '2em'}}>
         <h2>Toborzós</h2>
-            <div className="challengeItem">
+            <Link href="https://forms.gle/LmrxTpWAwAGgdXSz7" className="challengeItem">
             <div><b>Toborozz valakit a csapatodba!</b> - 1p - Ismételhető</div>
             <ul>
                 <li>Győzz meg valakit, hogy lépjen be a csapatodba!</li>
             </ul>
-            </div>
+            </Link>
             <br />
         </section>
 
         <section class="feature" style={{marginTop: '2em'}}>
         <h2>Szavazó állomások</h2>
+            <span>Ezek a feladatok várhatóan 10.28 délután nyílnak meg</span>
+
             <div className="challengeItem">
             <div><b>Látogass meg 3 szavazó állomást és szelfizz ott!</b> - 5p</div>
             <ul>
@@ -113,24 +116,26 @@ export default async function CategoryHomePage() {
         <section class="feature" style={{marginTop: '2em'}}>
         <h2>Kvíz</h2>
         <div>Itt válaszolni kell a kérdésekre pontért</div>
-            <div className="challengeItem">
+            <Link href="https://forms.gle/XumvS8LkskskDJNP9" className="challengeItem">
             <div><b>Melyik a két legtávolabbi szavazó állomás?</b> - 2p</div>
-            </div>
+            </Link>
             <br />
 
-            <div className="challengeItem">
+            <Link href="https://forms.gle/F5RB5zyhGdxfNj5p7" className="challengeItem">
             <div><b>Melyik a legnépszerűbb vidéki szavazó állomás?</b> - 2p</div>
-            </div>
+            </Link>
             <br />
 
-            <div className="challengeItem">
+            <Link href="https://forms.gle/aXzt2hWq2qx1s6T18" className="challengeItem">
             <div><b>Melyik a legnépszerűbb budapesti szavazó állomás?</b> - 2p</div>
-            </div>
+            </Link>
             <br />
         </section>
 
         <section class="feature" style={{marginTop: '2em'}}>
         <h2>Vicces</h2>
+            <span>Ezek a feladatok várhatóan 10.28 délután nyílnak meg</span>
+
             <div className="challengeItem">
             <div><b>Budapest Bingó (normál)</b> - 7p - Ismételhető</div>
             <ul>
