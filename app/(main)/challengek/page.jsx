c14cec6d58c50524904c6062b18459ada688fc24
domino-1 <78816259+domino-1@users.noreply.github.com>
2023-10-28 04:27:10 +0200

    challenge looks

--- app/(main)/challengek/page.jsx
+++ app/(main)/challengek/page.jsx
@@ -8,120 +8,154 @@ export default async function CategoryHomePage() {
         <h1 className="contentChallengek">Challengek</h1>
         <br />
         <section class="feature" style={{marginTop: '2em'}}>
-            <h2>Szavazás</h2>
+        <h2>Szavazás</h2>
+            <div className="challengeItem">
             <div><b>Szavazz!</b> - 1p</div>
             <ul>
                 <li>Élőben: Szelfi szavazás után</li>
                 <li>Online: Oszd meg, hogy szavaztál! A megosztott posztról csinálj egy screenshotot és küld be, vagy küld be a poszt linkjét</li>
                 <li>FONTOS: Tilos lefotózni a szavazólapot, vagy más szavazókat</li>
             </ul>
+            </div>
             <br />
 
+            <div className="challengeItem">
             <div><b>Hozz még egy szavazót!</b> - 2p (1p neki a "Szavazz!"-ért!) - Ismételhető</div>
             <ul>
                 <li>Élőben: Szelfizzetek együtt szavazás után!</li>
                 <li>Online: Osszátok meg mindketten, hogy szavaztatok! A megosztott posztrokról csináljatok egy screenshotot és küld be, vagy küld be a posztok linkjét</li>
                 <li>FONTOS: Tilos lefotózni a szavazólapot, vagy más szavazókat akik nem ti vagytok</li>
             </ul>
+            </div>
             <br />
 
+            <div className="challengeItem">
             <div><b>Első szavazó</b> - 3p</div>
             <ul>
                 <li>Élőben: Fotózkodj az Első Szavazó matricáddal</li>
             </ul>
+            </div>
             <br />
 
+            <div className="challengeItem">
             <div><b>Sulitúra</b> - 7p</div>
             <ul>
                 <li>Érjetek el minél több iskolát! A csapatodnak annyiszor 7 pont jár, ahány különböző iskolából vannak tagjai.</li>
                 <li>Online és Élőben: Ugyan az a rendszer mint a "Hozz még egy szavazót!"nál</li>
             </ul>
+            </div>
             <br />
         </section>
 
         <section class="feature" style={{marginTop: '2em'}}>
-            <h2>Toborzós</h2>
+        <h2>Toborzós</h2>
+            <div className="challengeItem">
             <div><b>Toborozz valakit a csapatodba!</b> - 1p - Ismételhető</div>
             <ul>
                 <li>Győzz meg valakit, hogy lépjen be a csapatodba!</li>
             </ul>
+            </div>
             <br />
         </section>
 
         <section class="feature" style={{marginTop: '2em'}}>
-            <h2>Szavazó állomások</h2>
+        <h2>Szavazó állomások</h2>
+            <div className="challengeItem">
             <div><b>Látogass meg 3 szavazó állomást és szelfizz ott!</b> - 5p</div>
             <ul>
                 <li>FONTOS: Figyelj oda, hogy ne legyenek felismerhetőek az emberek a képen! Tilos lefotózni a szavazólapot és más szavazókat</li>
                 <li>Ha megkérnek, hogy ne fotózzd le őket, akkor ne fotózzd le őket!</li>
             </ul>
+            </div>
             <br />
 
+            <div className="challengeItem">
             <div><b>CSAPATOS: Gyűjtsetek össze egy-egy képet Budapest minden szavazó állomásáról </b> - 20p</div>
             <ul>
                 <li>Dolgozzatok össze csapattársaiddal!</li>
             </ul>
+            </div>
             <br />
 
+            <div className="challengeItem">
             <div><b>CSAPATOS: Gyűjtsetek össze egy-egy képet Pest megye minden szavazó állomásáról </b> - 20p</div>
             <ul>
                 <li>Dolgozzatok össze csapattársaiddal!</li>
             </ul>
+            </div>
             <br />
 
+            <div className="challengeItem">
             <div><b>CSAPATOS: Gyűjtsetek össze egy-egy képet legalább 3 megye szavazó állomásairól </b> - 20p - Ismételhető</div>
             <ul>
                 <li>Dolgozzatok össze csapattársaiddal!</li>
             </ul>
+            </div>
             <br />
 
+            <div className="challengeItem">
             <div><b>CSAPATOS: Gyűjtsetek össze minden megyéből egy képet egy szavzó állomásról </b> - 100p</div>
             <ul>
                 <li>Dolgozzatok össze csapattársaiddal!</li>
             </ul>
+            </div>
             <br />
 
+            <div className="challengeItem">
             <div><b>CSAPATOS: Gyűjtsetek össze egy-egy képet legalább 60 szavazó állomásról! </b> - 200p</div>
             <ul>
                 <li>Dolgozzatok össze csapattársaiddal!</li>
             </ul>
+            </div>
             <br />
         </section>
 
         <section class="feature" style={{marginTop: '2em'}}>
-            <h2>Kvíz</h2>
-            <div>Itt válaszolni kell a kérdésekre pontért</div>
+        <h2>Kvíz</h2>
+        <div>Itt válaszolni kell a kérdésekre pontért</div>
+            <div className="challengeItem">
             <div><b>Melyik a két legtávolabbi szavazó állomás?</b> - 2p</div>
+            </div>
             <br />
 
+            <div className="challengeItem">
             <div><b>Melyik a legnépszerűbb vidéki szavazó állomás?</b> - 2p</div>
+            </div>
             <br />
 
+            <div className="challengeItem">
             <div><b>Melyik a legnépszerűbb budapesti szavazó állomás?</b> - 2p</div>
+            </div>
             <br />
         </section>
 
         <section class="feature" style={{marginTop: '2em'}}>
-            <h2>Vicces</h2>
+        <h2>Vicces</h2>
+            <div className="challengeItem">
             <div><b>Budapest Bingó (normál)</b> - 7p - Ismételhető</div>
             <ul>
                 <li>Látogassatok meg egy szavazó állomást és szelfizzetek ott!</li>
                 <li>Annyiszor 5p ahány bingója lett a csapatodnak</li>
             </ul>
+            </div>
             <br />
 
+            <div className="challengeItem">
             <div><b>Budapest Bingó (nehéz)</b> - 15p - Ismételhető</div>
             <ul>
                 <li>Látogassatok meg egy szavazó állomást és szavazzon ott valaki!</li>
                 <li>Annyiszor 15p ahány bingója lett a csapatodnak</li>
             </ul>
+            </div>
             <br />
 
+            <div className="challengeItem">
             <div><b>Érvelős</b></div>
             <ul>
                 <li>Gyűjts érveket, hogy miért megy el valaki szavazni -1p/érv</li>
                 <li>Gyűjts érveket, hogy miért NEM megy el valaki szavazni -1p/érv</li>
             </ul>
+            </div>
             <br />
         </section>
     </>)
